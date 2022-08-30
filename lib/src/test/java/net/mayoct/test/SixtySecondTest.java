27c1e8a844d062cef462c069f14e55df711bfeb7
Masahiko Funaki <mfunaki@mayoct.net>
2022-08-31 01:27:50 +0900

    Replace 40 w/ 60

--- lib/src/test/java/net/mayoct/test/SixtySecondTest.java
+++ lib/src/test/java/net/mayoct/test/SixtySecondTest.java
@@ -5,6 +5,6 @@ import org.junit.Test;
 public class SixtySecondTest {
     @Test
     public void sixtySecondTest() throws InterruptedException {
-        Thread.sleep(40 * 1000);
+        Thread.sleep(60 * 1000);
     }
 }
